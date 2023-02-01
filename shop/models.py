3daf5ea47ae5449a7512618143c2df61a7cf19cd
Kyle Farrugia <K.farrugia94@gmail.com>
2023-02-01 14:35:13 +0000

    added basic models and linked up to admin site

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('shop:category_list', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    description = models.CharField('description', max_length=200)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    created_date = models.DateTimeField("creation date")
+    last_updated = models.DateTimeField("last update date")
+
+    class Meta:
+        verbose_name_plural = 'products'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
